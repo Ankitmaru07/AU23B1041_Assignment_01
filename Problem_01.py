1ff892805bf015e22b49d65d9fe2ce9782395f65
Ankitmaru07 <165752522+Ankitmaru07@users.noreply.github.com>
2024-04-22 11:38:04 +0000

    firstproblemanser

--- Problem_01.py
+++ Problem_01.py
@@ -1,1 +1,19 @@
+def get_name():
+    global name
+    name=input("enter your name:")
+    return name
 
+def get_tshirt():
+    global name
+    get_name()
+    a=input("which brand do you want:")
+    brand=['adidas', 'peter england', 'calvin klein', 'zara', 'raymond']
+    for i in brand:
+      if i==a:
+        print("Hi", name , ",", "the brand you are looking for is available in our store.")
+        break
+      else:
+        print("Hi", name , ",", "the brand you are looking for is not available in our store.")
+        break
+
+get_tshirt()
