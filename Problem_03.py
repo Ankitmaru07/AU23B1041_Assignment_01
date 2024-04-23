4849a83d11619ee5c43b4f2d7b5857b59c0bf695
Ankitmaru07 <165752522+Ankitmaru07@users.noreply.github.com>
2024-04-23 10:25:40 +0000

    Ans.problem-3

--- Problem_03.py
+++ Problem_03.py
@@ -1,2 +1,16 @@
+def order(): 
+    stack=[] 
+a=int(input("enter the number of item")) 
+stack=[] 
+
+for i in range(1,a+1):
+    b=input("enter the order") 
+    stack.append(b) 
+    
+             
+for i in range(a):
+            print("The following order have been prepared",stack[i]) 
 
 
+for j in range(a):
+            print("The following order have been despatched",stack[j]) 
