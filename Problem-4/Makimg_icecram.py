7e3a43c8b577a67ea3473a37a7e4d11b954e4f9b
Ankitmaru07 <165752522+Ankitmaru07@users.noreply.github.com>
2024-05-04 21:08:44 +0530

    Create Makimg_icecram.py

---
+++ Problem-4/Makimg_icecram.py
@@ -0,0 +1,5 @@
+def add_topping(scoop_size, *toppings):
+        print(toppings," toppings to ",scoop_size, "scoops of ice cream.")
+
+def make_shake(type_of_shake):
+    print(type_of_shake," shake is being prepared.")
